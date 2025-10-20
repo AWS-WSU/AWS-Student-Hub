8e1880efbdabdb81cd81e7a344c956f30b1d30df
akrm al-hakimi <alhakimiakrmj@gmail.com>
2025-10-20 19:39:54 -0400

    api: explicit OPTIONS handler

--- backend/routes/verify.js
+++ backend/routes/verify.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { verifyUser } = require('../controllers/verifyController');
 
+router.options('/', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://www.prizeversity.com');
+  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.status(200).end();
+});
+
 router.post('/', verifyUser);
 
 module.exports = router;
