b6584d5e1d989cb4f48f17499fc64a9d3d2a52a9
akrm al-hakimi <alhakimiakrmj@gmail.com>
2026-05-17 18:17:28 -0400

    chore: trigger deploy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-  console.log(`📡 API available at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+  console.log(`API available at http://localhost:${PORT}`);
 });
